1c916575a26d53d9a4e3f1e243779d1683da5739
birdie <birdie@teslaCoil>
2014-04-27 11:02:34 -0600

    First commit

---
+++ receiveTM/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=receivetm
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/receivetm
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=receivetm.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/receivetm.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=receivetm
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/receivetm
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=receivetm.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/receivetm.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
